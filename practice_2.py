8f338b3b6e91d0906b3a7bd1c64513f54794f94f
icer7 <ryo0104777@gmail.com>
2019-01-12 13:42:03 +0900

    3

--- practice_2.py
+++ practice_2.py
@@ -8,14 +8,4 @@ print(name, type(is_ok))
 print(name, type(is_ok))
 print(name, type(is_ok))
 print(name, type(is_ok))
-print(name, type(is_ok))num = 1
-name = 'ice'
-is_ok = True
-
-print(num, type(num))
-print(name, type(name))
-print(name, type(is_ok))
-print(name, type(is_ok))
-print(name, type(is_ok))
-print(name, type(is_ok))
 print(name, type(is_ok))
