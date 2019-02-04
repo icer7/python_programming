bd983b223a00225adffc0d590f2825e8f24e8079
icer7 <ryo0104777@gmail.com>
2019-02-04 10:47:12 +0900

    practice

---
+++ practice2.py
@@ -0,0 +1,11 @@
+num = 1
+name = 'ice'
+is_ok = True
+
+print(num, type(num))
+print(name, type(name))
+print(name, type(is_ok))
+print(name, type(is_ok))
+print(name, type(is_ok))
+print(name, type(is_ok))
+print(name, type(is_ok))
